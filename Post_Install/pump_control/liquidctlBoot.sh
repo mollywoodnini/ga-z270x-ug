90a227694b2519f01841f72b162eb45a8a0855d4
chase <icedterminal@gmail.com>
2019-02-19 05:19:29 -0800

    Update liquidctlBoot.sh

--- Post_Install/pump_control/liquidctlBoot.sh
+++ Post_Install/pump_control/liquidctlBoot.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # just in case the device is not ready
+# if errors no big deal because its ready
 liquidctl initialize
-# set fans to quiet mode
-liquidctl set fan speed  20 25  32 38  40 50  52 70  65 100
-# set pump speed higher from default 60
-liquidctl set pump speed 80
+# set fans to generous speed in 5 steps
+liquidctl set fan speed  20 25  32 38  40 50  52 70  62 100
+# set pump speed to range in 2 steps
+liquidctl set pump speed 20 60 62 100
 # set lights
 liquidctl set logo color breathing ffffff
 liquidctl set ring color water-cooler
