0bcb8b7043069b3c4ac95c59351e38f77efe5319
icedterminal <icedterminal@gmail.com>
2019-02-21 03:02:04 -0800

    PATH fixup
    For those who don't add things to PATH. I didn't even think about it. This fix covers that.
    
    Also tweaked curves lights for basic use

--- Post_Install/pump_control/liquidctlBoot.sh
+++ Post_Install/pump_control/liquidctlBoot.sh
@@ -3,9 +3,9 @@
 # if errors no big deal because its ready
 liquidctl initialize
 # set fans to generous speed in 5 steps
-liquidctl set fan speed  20 25  32 38  40 50  52 70  62 100
+liquidctl set fan speed  20 25  32 40  42 52  52 70  60 100
 # set pump speed to range in 2 steps
-liquidctl set pump speed 20 60 62 100
+liquidctl set pump speed 20 60 60 100
 # set lights
-liquidctl set logo color breathing ffffff
-liquidctl set ring color water-cooler
+liquidctl set logo color fixed ffffff
+liquidctl set ring color fixed ffffff
