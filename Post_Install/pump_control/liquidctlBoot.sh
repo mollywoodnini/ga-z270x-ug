3067b1fd363a38b6fad7fe71dc0136d862e411f7
icedterminal <icedterminal@gmail.com>
2019-03-01 13:55:14 -0800

    Update liquidctlBoot.sh
    1hr overclock testing for high heat generation. Leveled off at 60c. Not too loud either.

--- Post_Install/pump_control/liquidctlBoot.sh
+++ Post_Install/pump_control/liquidctlBoot.sh
@@ -2,10 +2,10 @@
 # just in case the device is not ready
 # if errors no big deal because its ready
 liquidctl initialize
-# set fans to generous speed in 5 steps
-liquidctl set fan speed  20 25  32 40  42 52  52 70  60 100
+# set fans to aggressive speed in 5 steps
+liquidctl set fan speed  25 25  32 52  42 60  52 70  58 100
 # set pump speed to range in 2 steps
-liquidctl set pump speed 20 60 60 100
+liquidctl set pump speed 25 65 58 100
 # set lights
 liquidctl set logo color fixed ffffff
 liquidctl set ring color fixed ffffff
